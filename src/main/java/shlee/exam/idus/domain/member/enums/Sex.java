9e3f755a16f1e07f5e96e2b61a249bc39090124f
sh-mac <dltkdgns3435@naver.com>
2022-03-08 10:21:42 +0900

    IT/5: 성별 enum 작성

---
+++ src/main/java/shlee/exam/idus/domain/member/enums/Sex.java
@@ -0,0 +1,11 @@
+package shlee.exam.idus.domain.member.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Sex {
+    M,
+    F;
+}
