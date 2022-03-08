ffdb70b481339851ea550a222e5944e7dafaab0b
sh-mac <dltkdgns3435@naver.com>
2022-03-08 10:21:10 +0900

    IT/4: 사용자 생성 dto 설계 및 테스트케이스 작성

---
+++ src/main/java/shlee/exam/idus/domain/member/dto/request/PostMemberDto.java
@@ -0,0 +1,79 @@
+package shlee.exam.idus.domain.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import shlee.exam.idus.domain.member.entity.Member;
+import shlee.exam.idus.domain.member.enums.Sex;
+import shlee.exam.idus.global.exception.exceptions.RequestValidationException;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.regex.Pattern;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostMemberDto {
+    @Size(max = 20, message = "이름을 20글자 이내로 입력해주세요.")
+    @NotEmpty(message = "이름은 필수값 입니다.")
+    private String name;
+
+    @Size(max = 20, message = "별명을 30글자 이내로 입력해주세요.")
+    @NotEmpty(message = "별명은 필수값 입니다.")
+    private String nickname;
+
+    @Size(min = 10, message = "비밀번호를 10글자 이상으로 입력해주세요.")
+    @NotEmpty(message = "비밀번호는 필수값 입니다.")
+    private String password;
+
+    @Size(max = 20, message = "올바른 전화번호를 입력해주세요.")
+    @NotEmpty(message = "전화번호는 필수값 입니다.")
+    private String phone;
+
+    @Size(max = 100, message = "이메일을 100글자 이내로 입력해주세요.")
+    @NotEmpty(message = "이메일은 필수값 입니다.")
+    @Email(message = "올바른 형식의 이메일을 입력해주세요.")
+    private String email;
+
+    private String sex;
+
+    private void validation() {
+        String nameRule = "^[가-힣a-zA-Z ]*$";
+        String nicknameRule = "^[a-z]*$";
+        String passwordRule = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{10,}$";
+        String phoneRule = "^[0-9]*$";
+        String emailRule = "[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+";
+
+        if (!Pattern.matches(nameRule, name))
+            throw new RequestValidationException("이름은 한글, 영문 대소문자만 입력 가능합니다.");
+
+        if (!Pattern.matches(nicknameRule, nickname))
+            throw new RequestValidationException("별명은 영문 소문자만 입력 가능합니다.");
+
+        if (!Pattern.matches(passwordRule, password))
+            throw new RequestValidationException("비밀번호는 영문 대문자, 영문 소문자, 특수 문자, 숫자 각 1개 이상씩 포함해야 합니다.");
+
+        if (!Pattern.matches(phoneRule, phone))
+            throw new RequestValidationException("휴대폰번호는 '-' 제외 숫자만 입력해주세요.");
+
+        if (!Pattern.matches(emailRule, email))
+            throw new RequestValidationException("올바른 형식의 이메일을 입력해주세요.");
+    }
+
+    public Member toEntity() {
+        validation();
+        Sex sex = this.sex == null ? null : this.sex.equals("M") ? Sex.M : this.sex.equals("F") ? Sex.F : null;
+
+        return Member.builder()
+                .name(name)
+                .nickname(nickname)
+                .password(password)
+                .phone(phone)
+                .email(email)
+                .sex(sex)
+                .build();
+    }
+
+}
---
+++ src/test/java/shlee/exam/idus/domain/member/dto/request/PostMemberDtoTest.java
@@ -0,0 +1,164 @@
+package shlee.exam.idus.domain.member.dto.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import shlee.exam.idus.domain.member.entity.Member;
+import shlee.exam.idus.domain.member.enums.Sex;
+import shlee.exam.idus.global.exception.exceptions.RequestValidationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostMemberDtoTest {
+
+    private final String name = "이상훈";
+    private final String nickname = "karry";
+    private final String password = "Dltkdgns1@";
+    private final String phone = "01068283435";
+    private final String email = "dltkdgns3435@kakao.com";
+    private final String sex = "M";
+
+    @Nested
+    @DisplayName("사용자 생성 dto 검증")
+    class ValidationPostMemberDto {
+
+        @Nested
+        @DisplayName("정상 케이스")
+        class SuccessCase {
+            @Test
+            @DisplayName("정상적인 값이면 검증에 성공한다.")
+            void validation() {
+                //given
+                PostMemberDto postMemberDto = PostMemberDto.builder()
+                        .name(name)
+                        .nickname(nickname)
+                        .password(password)
+                        .phone(phone)
+                        .email(email)
+                        .sex(sex)
+                        .build();
+
+                //when
+                Member member = postMemberDto.toEntity();
+
+                //then
+                assertThat(member.getName()).isEqualTo(name);
+                assertThat(member.getNickname()).isEqualTo(nickname);
+                assertThat(member.getPassword()).isEqualTo(password);
+                assertThat(member.getPhone()).isEqualTo(phone);
+                assertThat(member.getEmail()).isEqualTo(email);
+                assertThat(member.getSex()).isEqualTo(Sex.valueOf(sex));
+            }
+        }
+
+        @Nested
+        @DisplayName("비정상 케이스")
+        class FailCase {
+            @Test
+            @DisplayName("이름이 한글, 영문 대소문자가 아니면 실패한다.")
+            void validationFailByName() {
+                //given
+                PostMemberDto postMemberDto = PostMemberDto.builder()
+                        .name("123")
+                        .nickname(nickname)
+                        .password(password)
+                        .phone(phone)
+                        .email(email)
+                        .sex(sex)
+                        .build();
+
+                //when
+                RequestValidationException exception = assertThrows(RequestValidationException.class, postMemberDto::toEntity);
+
+                //then
+                assertThat(exception.getMessage()).isEqualTo("이름은 한글, 영문 대소문자만 입력 가능합니다.");
+
+            }
+
+            @Test
+            @DisplayName("별명이 영문 소문자가 아니면 실패한다.")
+            void validationFailByNickname() {
+                //given
+                PostMemberDto postMemberDto = PostMemberDto.builder()
+                        .name(name)
+                        .nickname("KARRY")
+                        .password(password)
+                        .phone(phone)
+                        .email(email)
+                        .sex(sex)
+                        .build();
+
+                //when
+                RequestValidationException exception = assertThrows(RequestValidationException.class, postMemberDto::toEntity);
+
+                //then
+                assertThat(exception.getMessage()).isEqualTo("별명은 영문 소문자만 입력 가능합니다.");
+
+            }
+
+            @Test
+            @DisplayName("비밀번호가 영문 대문자, 영문 소문자, 특수 문자, 숫자 각 1개 이상씩 포함되지 않으면 실패한다.")
+            void validationFailByPassword() {
+                //given
+                PostMemberDto postMemberDto = PostMemberDto.builder()
+                        .name(name)
+                        .nickname(nickname)
+                        .password("1q2w3e4r")
+                        .phone(phone)
+                        .email(email)
+                        .sex(sex)
+                        .build();
+
+                //when
+                RequestValidationException exception = assertThrows(RequestValidationException.class, postMemberDto::toEntity);
+
+                //then
+                assertThat(exception.getMessage()).isEqualTo("비밀번호는 영문 대문자, 영문 소문자, 특수 문자, 숫자 각 1개 이상씩 포함해야 합니다.");
+
+            }
+
+            @Test
+            @DisplayName("휴대폰번호가 숫자가 아니면 실패한다.")
+            void validationFailByPhone() {
+                //given
+                PostMemberDto postMemberDto = PostMemberDto.builder()
+                        .name(name)
+                        .nickname(nickname)
+                        .password(password)
+                        .phone("010-6828-3435")
+                        .email(email)
+                        .sex(sex)
+                        .build();
+
+                //when
+                RequestValidationException exception = assertThrows(RequestValidationException.class, postMemberDto::toEntity);
+
+                //then
+                assertThat(exception.getMessage()).isEqualTo("휴대폰번호는 '-' 제외 숫자만 입력해주세요.");
+
+            }
+
+            @Test
+            @DisplayName("이메일 형식이 바르지 않으면 실패한다.")
+            void validationFailByEmail() {
+                //given
+                PostMemberDto postMemberDto = PostMemberDto.builder()
+                        .name(name)
+                        .nickname(nickname)
+                        .password(password)
+                        .phone(phone)
+                        .email("dltkdgns3435@kakao")
+                        .sex(sex)
+                        .build();
+
+                //when
+                RequestValidationException exception = assertThrows(RequestValidationException.class, postMemberDto::toEntity);
+
+                //then
+                assertThat(exception.getMessage()).isEqualTo("올바른 형식의 이메일을 입력해주세요.");
+
+            }
+        }
+    }
+}
