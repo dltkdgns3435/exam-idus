a3b0438c050e450b3eb6572a05d44fcd0c3fccb9
sh-mac <dltkdgns3435@naver.com>
2022-03-09 22:29:18 +0900

    IT/16: Swagger 라이브러리 및 config 추가

---
+++ src/main/java/shlee/exam/idus/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package shlee.exam.idus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket swaggerAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
+                .useDefaultResponseMessages(true);
+    }
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "X-AUTH-TOKEN", "header");
+    }
+    private SecurityContext securityContext() {
+        return SecurityContext
+                .builder()
+                .securityReferences(defaultAuth())
+                .operationSelector(operationContext -> operationContext.requestMappingPattern().startsWith("/"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("이상훈 아이디어스 과제")
+                .description("API 문서 입니다.")
+                .version("1.0")
+                .build();
+    }
+}
