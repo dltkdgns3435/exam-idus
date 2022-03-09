14c7c20712fbdb49bc0dbb5fe81e275f66e46135
sh-mac <dltkdgns3435@naver.com>
2022-03-09 22:20:33 +0900

    IT/15: deniedTokenRepository DI

--- src/main/java/shlee/exam/idus/config/SecurityConfig.java
+++ src/main/java/shlee/exam/idus/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import shlee.exam.idus.domain.member.repository.DeniedTokenRepository;
 import shlee.exam.idus.global.jwt.JwtTokenProvider;
 
 import java.util.Arrays;
@@ -22,6 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
+    private final DeniedTokenRepository deniedTokenRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -41,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().permitAll();
         http
-                .apply(new JwtSecurityConfig(jwtTokenProvider));
+                .apply(new JwtSecurityConfig(jwtTokenProvider, deniedTokenRepository));
     }
 
 
