5b9c3a5835d4ed2529f06c64e668633d47c7a1cc
sh-mac <dltkdgns3435@naver.com>
2022-03-09 22:19:51 +0900

    IT/14: deniedTokenRepository DI

--- src/main/java/shlee/exam/idus/config/JwtSecurityConfig.java
+++ src/main/java/shlee/exam/idus/config/JwtSecurityConfig.java
@@ -5,16 +5,18 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import shlee.exam.idus.domain.member.repository.DeniedTokenRepository;
 import shlee.exam.idus.global.jwt.JwtAuthFilter;
 import shlee.exam.idus.global.jwt.JwtTokenProvider;
 
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final JwtTokenProvider jwtTokenProvider;
+    private final DeniedTokenRepository deniedTokenRepository;
 
     @Override
     public void configure(HttpSecurity http){
-        JwtAuthFilter jwtAuthFilter = new JwtAuthFilter(jwtTokenProvider);
+        JwtAuthFilter jwtAuthFilter = new JwtAuthFilter(jwtTokenProvider, deniedTokenRepository);
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
