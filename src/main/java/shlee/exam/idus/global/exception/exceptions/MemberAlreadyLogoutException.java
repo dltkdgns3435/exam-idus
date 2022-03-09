4b3b5def03d5ee21d2a4a6c18520af18edf663e9
sh-mac <dltkdgns3435@naver.com>
2022-03-09 23:20:08 +0900

    IT/19: 기 로그아웃 회원 예외 추가

---
+++ src/main/java/shlee/exam/idus/global/exception/exceptions/MemberAlreadyLogoutException.java
@@ -0,0 +1,7 @@
+package shlee.exam.idus.global.exception.exceptions;
+
+public class MemberAlreadyLogoutException extends RuntimeException{
+    public MemberAlreadyLogoutException(String message) {
+        super(message);
+    }
+}
